834c88b67677dfa079519ba7c9e0d1bed2596f4a
mohan <mohansingh_thagunna@outlook.com>
2016-08-23 12:21:48 +0545

    tab cell

---
+++ ClubTracker/TabbarCell.swift
@@ -0,0 +1,19 @@
+//
+//  TabbarCell.swift
+//  ClubTracker
+//
+//  Created by Mohan Singh Thagunna on 8/20/16.
+//  Copyright © 2016 CreatuDevelopers. All rights reserved.
+//
+
+import UIKit
+
+class TabbarCell: UICollectionViewCell {
+
+    @IBOutlet weak var lblTitle: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
